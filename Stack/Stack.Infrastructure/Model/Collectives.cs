cb879dce36f2689eb14ace0a32f1536c4b423ce7
radoni02 <108626591+radoni02@users.noreply.github.com>
2024-03-30 21:17:12 +0100

    added Infrastructure project and models

---
+++ Stack/Stack.Infrastructure/Model/Collectives.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack.Infrastructure.Model;
+
+public class Collectives //for now it is mock check if implementation is required
+{
+}
---
+++ Stack/Stack.Infrastructure/Model/Wrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Stack.Infrastructure.Model; 
+public class Wrapper<T> where T : class//implement marker interface here
+{ 
+    [JsonPropertyName("backoff")]
+    public int? BackOff{ get; init; }
+    [JsonPropertyName("error_id")]
+    public int? ErrorId { get; init; }
+    [JsonPropertyName("error_message")]
+    public string? ErrorMessage { get; init; }
+    [JsonPropertyName("error_name")]
+    public string? ErrorName { get; init; }
+    [JsonPropertyName("has_more")]
+    public bool HasMore { get; init; }
+    [JsonPropertyName("items")]
+    public T Items { get; init; }
+    [JsonPropertyName("quota_max")]
+    public int QuotaMax{ get; init; }
+    [JsonPropertyName("quota_remaining")]
+    public int QuotaRemaining { get; init;} 
+}
