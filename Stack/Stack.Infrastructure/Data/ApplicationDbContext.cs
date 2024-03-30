f4e3abc577824962feaeea4a08ea03257d99be39
radoni02 <108626591+radoni02@users.noreply.github.com>
2024-03-30 21:51:22 +0100

    added applicationDbContext

---
+++ Stack/Stack.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stack.Infrastructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack.Infrastructure.Data;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Tag> tags { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
